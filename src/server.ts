3f3c39f06d68893175f7cb8eee31d673623066ad
eoTREMBALAkrl <117782983+eoTREMBALAkrl@users.noreply.github.com>
2024-09-25 13:29:58 -0300

    4

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,48 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler, ZodTypeProvider } from "fastify-type-provider-zod";
 
-const app = fastify().withTypeProvider<ZodTypeProvider>();
 import { createUsuarioRoutes } from "./routes/usuario/create-usuario";
+import { deleteUsuarioRoutes } from "./routes/usuario/delete-usuario";
+import { updateUsuarioRoutes } from "./routes/usuario/update-usuario";
+import { getUsuarioRoutes } from "./routes/usuario/get-usuario";
+
+import {createRemedioRoutes} from "./routes/remedio/create-remedio";
+import {deleteRemedioRoutes} from "./routes/remedio/delete-remedio";
+import {updateRemedioRoutes} from "./routes/remedio/update-remedio";
+import {getRemedioRoutes} from "./routes/remedio/get-remedio";
+
+import {createPrescricaoRoutes} from "./routes/prescricao/create-prescricao";
+import {deletePrescricaoRoutes} from "./routes/prescricao/delete-prescricao";
+import {updatePrescricaoRoutes} from "./routes/prescricao/update-prescricao";
+import {getPrescricaoRoutes} from "./routes/prescricao/get-prescricao";
+
+import {createHistoricoRoutes} from "./routes/historico/create-historico";
+import {deleteHistoricoRoutes} from "./routes/historico/delete-historico";
+import {updateHistoricoRoutes} from "./routes/historico/update-historico";
+import {getHistoricoRoutes} from "./routes/historico/get-historico";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
 app.register(createUsuarioRoutes);
+app.register(deleteUsuarioRoutes);
+app.register(updateUsuarioRoutes);
+app.register(getUsuarioRoutes);
+
+app.register(createRemedioRoutes);
+app.register(deleteRemedioRoutes);
+app.register(updateRemedioRoutes);
+app.register(getRemedioRoutes);
+
+app.register(createPrescricaoRoutes);
+app.register(deletePrescricaoRoutes);
+app.register(updatePrescricaoRoutes);
+app.register(getPrescricaoRoutes);
+
+app.register(createHistoricoRoutes);
+app.register(deleteHistoricoRoutes);
+app.register(updateHistoricoRoutes);
+app.register(getHistoricoRoutes);
+
 
 
 app.listen({
